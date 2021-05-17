77ec0a9590104906a658e38600bc7563bde2d901
Ian Arnold <ianarnold@pop-os.localdomain>
2021-05-17 15:58:46 -0300

    wip

---
+++ resources/views/taskManager/dashboard.blade.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+        <title>
+            MANY PROJECTS
+        </title>
+        <meta
+            content='width=device-width, initial-scale=1.0, shrink-to-fit=no'
+            name='viewport'/>
+        <!-- Fonts and icons -->
+        <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"/>
+        <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/main.css') }} ">
+    </head>
+    <body class="">
+        <div class="wrapper ">
+            <div
+                class="sidebar"
+                data-color="purple"
+                data-background-color="white"
+                data-image="./resources/img/sidebar-1.jpg">
+                <div class="logo">
+                    <a href="http://www.creative-tim.com" class="simple-text logo-normal">
+                        CRUD PHP
+                    </a>
+                </div>
+                <div class="sidebar-wrapper">
+                    <ul class="nav">
+                        <li class="nav-item active ">
+                            <a class="nav-link" href="#">
+                                <i class="material-icons">content_paste</i>
+                                <p>Tabelas</p>
+                            </a>
+                        </li>
+                        <li class="nav-item active ">
+                            <a class="nav-link" href="#">
+                                <i class="material-icons">content_paste</i>
+                                <p>Formulário cadastro</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="main-panel">
+                <div class="content">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="table-responsive">
+                                            <div class="col-md-12">
+                                                <div class="card card-plain">
+                                                    <div class="card-header card-header-primary">
+                                                        <h4 class="card-title mt-0">
+                                                            Tabelas de usuários</h4>
+                                                        <p class="card-category">
+                                                            Todos usuários cadastrados</p>
+                                                    </div>
+                                                    <div class="card-body">
+                                                        <div class="table-responsive">
+                                                            <table class="table table-hover">
+                                                                <thead class="">
+                                                                    <th>
+                                                                        ID
+                                                                    </th>
+                                                                    <th>
+                                                                        Nome
+                                                                    </th>
+                                                                    <th>
+                                                                        País
+                                                                    </th>
+                                                                    <th>
+                                                                        Cidade
+                                                                    </th>
+                                                                    <th>
+                                                                        Salario
+                                                                    </th>
+                                                                </thead>
+                                                                <tbody>
+                                                                    <tr>
+                                                                        <td>
+                                                                            1
+                                                                        </td>
+                                                                        <td>
+                                                                            Dakota Rice
+                                                                        </td>
+                                                                        <td>
+                                                                            Niger
+                                                                        </td>
+                                                                        <td>
+                                                                            Oud-Turnhout
+                                                                        </td>
+                                                                        <td>
+                                                                            $36,738
+                                                                        </td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td>
+                                                                            2
+                                                                        </td>
+                                                                        <td>
+                                                                            Minerva Hooper
+                                                                        </td>
+                                                                        <td>
+                                                                            Curaçao
+                                                                        </td>
+                                                                        <td>
+                                                                            Sinaai-Waas
+                                                                        </td>
+                                                                        <td>
+                                                                            $23,789
+                                                                        </td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td>
+                                                                            3
+                                                                        </td>
+                                                                        <td>
+                                                                            Sage Rodriguez
+                                                                        </td>
+                                                                        <td>
+                                                                            Netherlands
+                                                                        </td>
+                                                                        <td>
+                                                                            Baileux
+                                                                        </td>
+                                                                        <td>
+                                                                            $56,142
+                                                                        </td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td>
+                                                                            4
+                                                                        </td>
+                                                                        <td>
+                                                                            Philip Chaney
+                                                                        </td>
+                                                                        <td>
+                                                                            Korea, South
+                                                                        </td>
+                                                                        <td>
+                                                                            Overland Park
+                                                                        </td>
+                                                                        <td>
+                                                                            $38,735
+                                                                        </td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td>
+                                                                            5
+                                                                        </td>
+                                                                        <td>
+                                                                            Doris Greene
+                                                                        </td>
+                                                                        <td>
+                                                                            Malawi
+                                                                        </td>
+                                                                        <td>
+                                                                            Feldkirchen in Kärnten
+                                                                        </td>
+                                                                        <td>
+                                                                            $63,542
+                                                                        </td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td>
+                                                                            6
+                                                                        </td>
+                                                                        <td>
+                                                                            Mason Porter
+                                                                        </td>
+                                                                        <td>
+                                                                            Chile
+                                                                        </td>
+                                                                        <td>
+                                                                            Gloucester
+                                                                        </td>
+                                                                        <td>
+                                                                            $78,615
+                                                                        </td>
+                                                                    </tr>
+                                                                </tbody>
+                                                            </table>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
